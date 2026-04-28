5ebe0f86ef2af655a700591b54a478b93032eb40
Aehsun Hafeez <73653865+Aehsun-Hafeez@users.noreply.github.com>
2026-04-28 20:23:14 +0000

    basics and datatypes

---
+++ 01-basics/01-variables.js
@@ -0,0 +1,13 @@
+const accountId = 123
+let accountEmail = 'aehsun@gmail.com'
+var accountPassword = '123'
+accountCity = 'Sialkot'
+let accountState
+
+//accountId = 2         not allowed
+accountEmail = 'naushy05@gmail.com'
+accountPassword = '321'
+accountCity = 'Lahore'
+
+console.table([accountEmail, accountPassword, accountCity, accountState])
+
---
+++ 01-basics/02-datatypes.js
@@ -0,0 +1,9 @@
+"use strict"        //treat entire code as new version
+//alert(5+5)        //it works on browser
+console.log('Aehsun')
+console.log(4+4)
+console.log(typeof "Aehsun")
+console.log(typeof 4)
+console.log(typeof undefined)
+console.log(typeof null)
+
