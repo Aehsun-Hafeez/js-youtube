d84a92552b49d840e445f3fb492e901b98f89e7e
Aehsun Hafeez <73653865+Aehsun-Hafeez@users.noreply.github.com>
2026-05-07 23:10:52 +0000

    operation lesson number 4

---
+++ 01-basics/04-operations.js
@@ -0,0 +1,21 @@
+// let value = 3
+// let negValue = -value
+// console.log(negValue)
+
+// console.log(2**3)
+
+// let str1 = "Aehsun "
+// let str2 = "Hafeez"
+// let str3 = str1+str2
+// console.log(str3)
+
+//problems
+console.log("1" + 2)        //12
+console.log(1 + "2")        //12
+console.log("1" + 2 + 2)    //122
+console.log(1 + 2 + "2")    //32
+console.log((3+4) * 5 % 3)  //2
+console.log(true)           //true
+console.log(+true)          //1
+// console.log(true+)          //error
+console.log(+"")
